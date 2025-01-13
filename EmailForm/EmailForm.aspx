77e664f482a54283d718c71d197b7e294b1b3e63
Sbx-404 <sbsumit404@gmail.com>
2025-01-13 11:25:31 +0530

    Add project files.

---
+++ EmailForm/EmailForm.aspx
@@ -0,0 +1,24 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="EmailForm.aspx.cs" Inherits="EmailForm.EmailForm" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h2>Contact Us</h2>
+            <asp:TextBox runat="server" ID="txtName" Placeholder="Your Name" CssClass="form-control" />
+            <br />
+            <asp:TextBox runat="server" ID="txtEmail" Placeholder="Your Email" CssClass="form-control" />
+            <br />
+            <asp:TextBox runat="server" ID="txtMessage" TextMode="MultiLine" Placeholder="Your Message" CssClass="form-control" Rows="5" />
+            <br />
+            <asp:Button runat="server" ID="btnSubmit" Text="Send Message" CssClass="btn btn-primary" OnClick="btnSubmit_Click" />
+
+        </div>
+    </form>
+</body>
+</html>
---
+++ EmailForm/EmailForm.aspx.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using MimeKit;
+using MailKit.Net.Smtp;
+
+namespace EmailForm
+{
+    public partial class EmailForm : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+            string name = txtName.Text;
+            string email = txtEmail.Text;
+            string message = txtMessage.Text;
+
+            if(!string.IsNullOrWhiteSpace(name) && !string.IsNullOrWhiteSpace(email) && !string.IsNullOrWhiteSpace(message))
+            {
+                string connectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+                SqlConnection con = new SqlConnection(connectionString);
+                string query = @"INSERT INTO [dbo].[EmailForm]
+            ([name]
+           ,[email]
+           ,[message])
+     VALUES
+           (@name, @email, @message)";
+                SqlCommand cmd = new SqlCommand(query, con);
+                
+                try
+                {
+                    var emailMessage = new MimeMessage();
+                    emailMessage.From.Add(new MailboxAddress("shiva","sbsumit404@gmail.com"));
+                    emailMessage.To.Add(new MailboxAddress("satyam",email));
+                    emailMessage.Subject = "New Contact Form Submission";
+                    emailMessage.Body = new TextPart("plain")
+                    {
+                        Text = $"Name: {name}\nEmail: {email}\nMessage: {message}"
+                    };
+
+                    var client = new SmtpClient();
+                    client.Connect("smtp.gmail.com", 587, false);
+                    client.Authenticate("sbsumit404@gmail.com", "nloh ujgq ephv cknk "); // this password is APP SPECFIC PASSWORD
+                    client.Send(emailMessage);
+                    //client.Dispose();
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                    con.Close();
+                    client.Disconnect(true);
+
+
+
+                }
+                catch (Exception err)
+                {
+                    Response.Write("<script>alert(email not save)</script>");
+                }
+
+            }
+
+        }
+    }
+}
